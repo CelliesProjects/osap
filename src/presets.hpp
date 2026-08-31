28f80a0068830b3d7253c843d627398d14dc1b11
Cellie <CelliesProjects@users.noreply.github.com>
2026-08-31 20:11:19 +0200

    Improved: presets

--- src/presets.hpp
+++ src/presets.hpp
@@ -26,19 +26,19 @@ constexpr struct source preset[]{
     {"XXL Stenders", "http://streams.robstenders.nl:8063/bonanza_mp3"},
     {"192 Radio Nederland", "http://192radio.stream-server.nl/stream"},
     {"80s Hitradio Amsterdam", "http://s22.myradiostream.com:7728/"},
+    {"Grolloo Radio", "https://de4.streamingpulse.com/ssl/7053"},
     {"Amsterdam Funk Channel", "https://stream.afc.fm/"},
-    {"Jazz Radio Soul Food by DJ Philgood", "http://jazz-wr12.ice.infomaniak.ch/jazz-wr12-128.mp3"},
+    {"Jazz Radio Soul", "http://jazz-wr12.ice.infomaniak.ch/jazz-wr12-128.mp3"},
     {"Disco Mix", "https://play.discomix.ro/8002/stream"},
     {"Absoluut FM", "http://absoluutfm.stream.laut.fm/absoluutfm"},
     {"Record DiscoFunk", "https://radiorecord.hostingradio.ru/discofunk96.aacp"},
     {"RadioEins", "http://radioeins.de/stream"},
-    {"ZUN radio", "https://icecast9.play.cz/zun192.mp3"},
-    {"Tekno1", "https://tekno1.radioca.st/listen.pls"},
-    {"Techno on Radio", "https://0n-techno.radionetz.de/0n-techno.aac"},
-    {"Record TECHNO", "https://radiorecord.hostingradio.ru/techno96.aacp"},
-    {"Kaaosradio 24h", "http://stream.kaaosradio.fi:8000/stream2"},
+    {"Planet 90", "https://planet90.beheerstream.nl/8010/stream"},
+    {"Tekno1", "https://tekno1.radioca.st/;"},
+    {"Hits 1 Algerie", "https://radio12.pro-fhi.net/listen/whmnrlow/stream"},
     {"bollywooddance", "https://nl4.mystreaming.net/uber/bollywooddance/icecast.audio"},
     {"bollywood", "http://2.mystreaming.net:80/er/bollywood/icecast.audio"},
+
 };
 
 constexpr const uint8_t NUMBER_OF_PRESETS = sizeof(preset) / sizeof(source);
